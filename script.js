7f336b772ede83fa7b9ec7c6a0c64732a3f5eb1f
Sneldo <tsneldo@comcast.net>
2024-11-08 13:44:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function show(var1) {
-	document.getElementsByClassName('show')[0].className='hide';
-  
-  document.getElementById(var1).className='show';
-}
 
-document.getElementById('divvy1').style.display = 'none';
-document.getElementById('divvy1').style.display = 'none';
-document.getElementById('divvy1').style.display = 'none';
-
-document.getElementById('var1').style.display = 'block';
